75863da8c2577f2a6dcd73f738cb2189c054068c
Pavel SHyraev <skyhobbit@yandex.ru>
2022-06-18 21:59:46 +0300

    feat: create layout

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import style from './Card.module.css'
+
+const Card = () => {
+  return (
+    <div className={style.card}>
+      <div className={style.card__wrapper}></div>
+    </div>
+  )
+}
+export default Card
